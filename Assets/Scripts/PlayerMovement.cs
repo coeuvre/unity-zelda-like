e90b632ade068d376aaf3b4b83c70d3347b7eeb5
coeuvre <coeuvre@gmail.com>
2019-08-14 22:53:27 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody2D;
+    public float speed;
+
+    private Vector3 _movement;
+    
+    void Update()
+    {
+        _movement = Vector3.zero;
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+        _movement = _movement.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.MovePosition(transform.position + speed * Time.fixedDeltaTime * _movement);
+    }
+}
