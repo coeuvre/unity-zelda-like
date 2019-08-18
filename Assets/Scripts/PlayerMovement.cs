119d1df6aeb7ab8b2df4eb5fd105c85c129b6df3
Coeuvre Wong <coeuvre@gmail.com>
2019-08-18 21:11:47 +0800

    4 way Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,27 @@ public class PlayerMovement : MonoBehaviour
 {
     public new Rigidbody2D rigidbody2D;
     public float speed;
+    public Animator animator;
 
     private Vector3 _movement;
-    
+
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Velocity = Animator.StringToHash("Velocity");
+
     void Update()
     {
         _movement = Vector3.zero;
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
         _movement = _movement.normalized;
+        var velocity = _movement.sqrMagnitude;
+        if (velocity > 0)
+        {
+            animator.SetFloat(Horizontal, _movement.x);
+            animator.SetFloat(Vertical, _movement.y);
+        }
+        animator.SetFloat(Velocity, velocity);
     }
 
     private void FixedUpdate()
